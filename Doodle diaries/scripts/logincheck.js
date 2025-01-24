9f1a8282b8b9dcff1e84e997fb1baa7e7b9035cd
Goosedavoh <122133591+Goosedavoh@users.noreply.github.com>
2025-01-23 20:50:31 -0600

    Delete Doodle diaries directory

--- Doodle diaries/scripts/logincheck.js
+++
@@ -1,8 +0,0 @@
-if (sessionStorage.getItem("loggedIn") === "true") {
-    alert("Signed in");
-    // Continue with the rest of the script
-}
-else {
-    alert("Not Signed in");
-    window.location.href = "../pages/signin.html";
-}
